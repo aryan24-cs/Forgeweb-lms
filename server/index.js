19fd387573910f554ea1dc8a19d84586aecd1c2b
aryan24-cs <aryan.nda.2163@gmail.com>
2026-02-27 23:05:01 +0530

    .

--- server/index.js
+++ server/index.js
@@ -68,4 +68,4 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/settings', settingsRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
